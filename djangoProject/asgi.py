f1f93a8d63afb643c4d0040e18b061d16ef5868f
Wangziming <2574411340@qq.com>
2023-05-11 09:54:52 +0800

    增加websocket配置

--- djangoProject/asgi.py
+++ djangoProject/asgi.py
@@ -9,8 +9,21 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+import djangoProject.urls
+application = ProtocolTypeRouter(
+    {
+        'http': django_asgi_app,
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(djangoProject.urls.websocket_urlpatterns))
+        ),
+    }
+)
